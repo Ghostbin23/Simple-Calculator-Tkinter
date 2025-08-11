d8bc009e6fc573382f7c3a14e5e7fd7446d8a8dd
Ghostbin32 <logty543@gmail.com>
2025-08-11 16:43:15 +0800

    simplified all lines to have no newlines

--- Calculator.py
+++ Calculator.py
@@ -27,30 +27,14 @@ root.geometry("270x150")
 display = StringVar()
 entry = Entry(root, textvariable=display)
 entry.grid(columnspan=4, ipadx=70)
-buttons = [
-    ('1', 2, 0), ('2', 2, 1), ('3', 2, 2),
-    ('4', 3, 0), ('5', 3, 1), ('6', 3, 2),
-    ('7', 4, 0), ('8', 4, 1), ('9', 4, 2),
-    ('0', 5, 1), ('.', 6, 0)
-]
+buttons = [('1', 2, 0), ('2', 2, 1), ('3', 2, 2), ('4', 3, 0), ('5', 3, 1), ('6', 3, 2), ('7', 4, 0), ('8', 4, 1), ('9', 4, 2), ('0', 5, 1), ('.', 6, 0)]
 for (text, row, col) in buttons:
-    Button(root, text=text, fg='black', bg='light green',
-           command=lambda x=text: press(x), height=1, width=7)\
-        .grid(row=row, column=col)
-ops = [
-    ('+', 2, 3), ('-', 3, 3), ('*', 4, 3), ('/', 5, 3),
-    ('^', 6, 3), ('(', 5, 0), (')', 5, 2)
-]
+    Button(root, text=text, fg='black', bg='light green', command=lambda x=text: press(x), height=1, width=7).grid(row=row, column=col)
+ops = [('+', 2, 3), ('-', 3, 3), ('*', 4, 3), ('/', 5, 3), ('^', 6, 3), ('(', 5, 0), (')', 5, 2)]
 for (text, row, col) in ops:
     cmd = lambda x=text: press('**' if x == '^' else x)
-    Button(root, text=text, fg='black', bg='light green',
-           command=cmd, height=1, width=7)\
-        .grid(row=row, column=col)
-specials = [
-    ('=', 6, 2, equal), ('Clear', 6, 1, clear)
-]
+    Button(root, text=text, fg='black', bg='light green', command=cmd, height=1, width=7).grid(row=row, column=col)
+specials = [('=', 6, 2, equal), ('Clear', 6, 1, clear)]
 for (text, row, col, func) in specials:
-    Button(root, text=text, fg='black', bg='red',
-           command=func, height=1, width=7)\
-        .grid(row=row, column=col)
+    Button(root, text=text, fg='black', bg='red', command=func, height=1, width=7).grid(row=row, column=col)
 root.mainloop()
